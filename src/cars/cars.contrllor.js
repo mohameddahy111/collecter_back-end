bfa0640855906e8e6b04f755b234a3182533985a
mohameddahy dahy <mohameddahy111@gmail.com>
2024-02-07 22:09:18 +0200

    apdet

--- src/cars/cars.contrllor.js
+++ src/cars/cars.contrllor.js
@@ -6,9 +6,9 @@ export const addCar = errorHandler(async (req, res, next) => {
   const {name} = req.body;
   const isExist = await Cars.findOne({name: name});
   if (isExist) {
-    return next(new AppError({message:"الاسم مستخدم"}, 401));
+    return next(new AppError({message: "الاسم مستخدم"}, 401));
   }
- await Cars.insertMany(req.body);
+  await Cars.insertMany(req.body);
 
   res.send({message: "تم اضافة براد جديد"}).status(201);
 });
@@ -21,19 +21,19 @@ export const getAllCars = errorHandler(async (req, res, next) => {
   res.send({cars}).status(200);
 });
 export const addProdect = errorHandler(async (req, res, next) => {
-    const { id } = req.params
-    const {name} = req.body
-    const fintItem = await Cars.findOne({ _id: id });
-    fintItem.Payload.map(async(item) => {
-        if (item.name != name) {
-            const list = fintItem.Payload.push(req.body)
-            await Cars.findByIdAndUpdate(id , {Payload : list} , {new: true})
-        } else {
-            
-        }
-    })
-
-    
-    res.send({fintItem}).status(200);
+  const {id} = req.params;
+  const {name} = req.body;
+  const findtItem = await Cars.findOne({_id: id});
+  const isExist = findtItem.Payload.find((x) => x.name == name);
+  if (!isExist) {
+    findtItem.Payload.push(req.body);
+    findtItem.save();
+    res.send({message: "تم اضافة منتج"}).status(201);
+  } else {
+    isExist.width = isExist.width + req.body.width;
+    isExist.price = isExist.price + req.body.price;
+    isExist.count = isExist.count + req.body.count;
+    findtItem.save();
+    res.send({message: "تم تعديل المنتح"}).status(200);
+  }
 });
-
--- src/cars/cars.schema.js
+++ src/cars/cars.schema.js
@@ -22,10 +22,19 @@ const carsSchema = new mongoose.Schema(
     ],
     drive_mony: {type: Number},
     totle_Price: {type: Number, default: 0},
+    totle_Prodect_value: {type: Number, default: 0},
+    totle_cost_value: {type: Number, default: 0},
     totle_width: {type: Number, default: 0}
   },
   {timestamps: true}
 );
+
+carsSchema.pre("save", function () {
+  this.totle_Prodect_value = this.Payload.reduce((a, c) => a + c.price, 0);
+  this.totle_cost_value = this.cost.reduce((a, c) => a + c.price, 0);
+  this.totle_width = this.Payload.reduce((a, c) => a + c.width, 0);
+  this.totle_Price =this.totle_Prodect_value+ this.totle_cost_value 
+});
 
 const Cars = mongoose.model("Cars", carsSchema);
 export default Cars;
