2e542d218d4030861eb3287327cd3787a9ef451b
mohameddahy dahy <mohameddahy111@gmail.com>
2024-02-14 23:37:51 +0200

    user update

--- src/login/user.controller.js
+++ src/login/user.controller.js
@@ -4,25 +4,25 @@ import {AppError} from "../utils/AppError.js";
 import jwt from "jsonwebtoken";
 
 export const login = errorHandler(async (req, res, next) => {
-  const {email, password} = req.body;
-  const findEmail = await User.findOne({email: email});
+  const {phone, password} = req.body;
+  const findEmail = await User.findOne({phone: phone});
   if (!findEmail) {
-    return next(new AppError("البريد الاكتروني غير صحيح", 402));
+    return next(new AppError("رقم الهاتف غير صحيح", 402));
   }
   if (findEmail.password !== password) {
     return next(new AppError(" كلمة المرور غير صحيح", 401));
   }
-  await User.findByIdAndUpdate({_id: findEmail._id}, {_isActive: true});
+  await User.findByIdAndUpdate( findEmail._id, {_isActive: true});
   const token = jwt.sign({id: findEmail._id}, process.env.TOKEN);
   res.send({message: `مرحبا ${findEmail.name}`,user_info :findEmail, token}).status(200);
 });
 
 // add user
 export const sigin = errorHandler(async (req, res, next) => {
-  const {email} = req.body;
-  const isExist = await User.findOne({email: email});
+  const {phone} = req.body;
+  const isExist = await User.findOne({phone});
   if (isExist) {
-    return next(new AppError(`البريد  الالكتروني مستخدم`, 401));
+    return next(new AppError(`الهاتف   مستخدم`, 401));
   }
   await User.insertMany(req.body);
   res.send({message: `تم اضافة مستخدم جديد`}).status(201);
